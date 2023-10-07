553eede69aa3f6cfa04abea56dacb47a99af074a
Nelly Z <nellyzulkiflee@gmail.com>
2023-10-07 18:49:09 +0800

    Uploaded data files

--- src/algorithmTester.java
+++ src/algorithmTester.java
@@ -95,6 +95,7 @@ public class algorithmTester {
         makeCSV.CSVprinter(vertexCounts, "vertice_counts.csv");
         makeCSV.CSVprinter(edgeCounts, "edge_counts.csv");
         makeCSV.CSVprinter(dijkstraArrayTimes, "dijkstra_array_times.csv");
+        makeCSV.CSVprinter(dijkstraArrayTimes, "dijkstra_list_times.csv");
     }
 
     private static void empiricalTestSparse(int maxVertices, int maxWeight, int graphCount, int averageTimes)
